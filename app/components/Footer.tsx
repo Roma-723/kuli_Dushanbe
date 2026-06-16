4e493d5be645cc7d80535f53e1c21eae419d30bc
Roma-723 <romajon.ll8@gmail.com>
2026-06-16 13:38:23 +0500

    firsName

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -18,6 +18,7 @@ export default function Footer() {
                 height={55}
                 className="rounded-lg object-contain"
               />
+
               
             </div>
             <div>
